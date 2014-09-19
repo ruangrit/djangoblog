a7f755badbd337c5f7110842eef2b2115f0cad3e
Rito <rito@MBPs-MacBook-Pro.local>
2014-09-19 13:14:42 +0700

    redirec success

--- blogengin/views.py
+++ blogengin/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, get_object_or_404
 from blogengin.models import Post, Comment, Category
 from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from blogengin.form import PostForm 
+from pprint import pprint
 # Create your views here.
 def list(request, cid):
 	posts = Post.objects.filter(cid=cid).order_by('-created')
-	return render(request, 'blogengin/list.html', {'posts': posts})
+	cat = Category.objects.get(cid=cid)
+	my_record = Category.objects.get(cid=1)
+	form = PostForm(instance=my_record)
+	pprint(vars(form))
+	return render(request, 'blogengin/list.html', {'posts': posts, 'cat': cat})
 
 def index(request):
 	cats = Category.objects.order_by('title')
@@ -16,13 +22,15 @@ def post(request, slug):
 	return render(request, 'blogengin/post.html', {'post': post})
 
 def post_form(request):
-	if request.method == 'POST':
-		
-		form = PostForm(request.POST)
-		if form.is_valid():
-			form.save()	
-			
-	return render(request, 'blogengin/post_add.html', {'form': PostForm})
+
+	form = PostForm(request.POST)
+	if form.is_valid():
+		#pprint(vars(form))
+		print form.cleaned_data[body]
+		new_post = form.save()
+
+		return HttpResponseRedirect(reverse(list, args=(new_post.cid_id,)))
+	return render(request, 'blogengin/post_add.html', {'form': form})
 
 
 
